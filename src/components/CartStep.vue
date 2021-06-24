7839a7c996ba35abfb2269472440ea2a403bcb3a
happy9990929 <lala9990929@gmail.com>
2021-06-24 23:08:48 +0800

    補大寫檔案

---
+++ src/components/CartStep.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="d-flex align-items-center justify-content-center flex-wrap my-5">
+    <div class="d-flex">
+      <div :class="['step', cartActive]">1</div>
+      <div :class="['stepText', cartActive]">確認購物車</div>
+    </div>
+    <div class="stepArrow d-none d-md-block">
+      <i class="fas fa-angle-right"></i>
+    </div>
+    <div class="d-flex">
+      <div :class="['step', infoActive]">2</div>
+      <div :class="['stepText', infoActive]">填寫資料</div>
+    </div>
+    <div class="stepArrow d-none d-md-block">
+      <i class="fas fa-angle-right"></i>
+    </div>
+    <div class="d-flex">
+      <div :class="['step', checkoutActive]">3</div>
+      <div :class="['stepText', checkoutActive]">確認訂單</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    cartActive() {
+      return this.$route.name === "Cart" ? "active" : "";
+    },
+    infoActive() {
+      return this.$route.name === "Info" ? "active" : "";
+    },
+    checkoutActive() {
+      return this.$route.name === "Checkout" ? "active" : "";
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.step {
+  font-size: 1.2rem;
+  color: #000;
+  background-color: #b7b7b7;
+  width: 2rem;
+  height: 2rem;
+  line-height: 2rem;
+  border-radius: 100%;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  &.active {
+    background-color: #feecba;
+  }
+}
+.stepText {
+  color: #b7b7b7;
+  font-size: 1.2rem;
+  border-bottom: 2px solid;
+  margin: 0 0.6rem;
+  &.active {
+    color: #feecba;
+    border-color: #feecba;
+  }
+}
+.stepArrow {
+  font-size: 1.8rem;
+  color: #b7b7b7;
+  margin: 0 1.5rem;
+}
+</style>
---
+++ src/views/frontend/Info.vue
@@ -0,0 +1,153 @@
+<template>
+  <main>
+    <banner />
+    <cart-step />
+    <div class="container mt-5">
+      <validation-observer v-slot="{ invalid }">
+        <form>
+          <div class="row">
+            <div class="col-12 col-md-6">
+              <div class="form-group">
+                <validation-provider rules="required" v-slot="{ errors, classes }">
+                  <label for="userName">姓名</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="userName"
+                    :class="classes"
+                    name="姓名"
+                    v-model="form.name"
+                  />
+                  <span class="invalid-feedback">{{ errors[0] }}</span>
+                </validation-provider>
+              </div>
+              <div class="form-group">
+                <validation-provider rules="required|min:8" v-slot="{ errors, classes }">
+                  <label for="tel">電話</label>
+                  <input
+                    id="tel"
+                    type="tel"
+                    class="form-control"
+                    :class="classes"
+                    name="電話"
+                    v-model="form.tel"
+                  />
+                  <span class="invalid-feedback">{{ errors[0] }}</span>
+                </validation-provider>
+              </div>
+              <div class="form-group">
+                <validation-provider rules="required|email" v-slot="{ errors, classes }">
+                  <label for="email">E-mail</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    :class="classes"
+                    name="email"
+                    v-model="form.email"
+                  />
+                  <span class="invalid-feedback">{{ errors[0] }}</span>
+                </validation-provider>
+              </div>
+              <div class="form-group">
+                <validation-provider rules="required" v-slot="{ errors, classes }">
+                  <label for="address">地址</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="address"
+                    :class="classes"
+                    name="地址"
+                    v-model="form.address"
+                  />
+                  <span class="invalid-feedback">{{ errors[0] }}</span>
+                </validation-provider>
+              </div>
+            </div>
+            <div class="col-12 col-md-6">
+              <div class="form-group">
+                <validation-provider rules="required" v-slot="{ errors, classes }">
+                  <label for="payment">付款方式</label>
+                  <select
+                    id="payment"
+                    class="form-control"
+                    :class="classes"
+                    name="付款方式"
+                    v-model="form.payment"
+                  >
+                    <option value="" disabled>
+                      請選擇付款方式
+                    </option>
+                    <option v-for="item in pay" :key="item" :value="item">{{ item }}</option>
+                  </select>
+                  <span class="invalid-feedback">{{ errors[0] }}</span>
+                </validation-provider>
+              </div>
+              <div class="form-group">
+                <label for="msg">留言</label>
+                <textarea
+                  type="text"
+                  class="form-control"
+                  id="msg"
+                  rows="8"
+                  v-model="form.message"
+                ></textarea>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between my-3">
+            <router-link to="/cart" class="outlineBtn">
+              <span class="angleLineRight mr-2"></span>
+              上一步
+            </router-link>
+            <button type="button" @click="submitForm" class="outlineBtn" :disabled="invalid">
+              下一步
+              <span class="angleLineLeft ml-2"></span>
+            </button>
+          </div>
+        </form>
+      </validation-observer>
+    </div>
+  </main>
+</template>
+
+<script>
+import banner from "@/components/Banner.vue";
+import cartStep from "@/components/CartStep.vue";
+
+export default {
+  components: {
+    banner,
+    cartStep
+  },
+  data() {
+    return {
+      form: {
+        name: "",
+        tel: "",
+        email: "",
+        address: "",
+        payment: "",
+        message: ""
+      },
+      pay: ["WebATM", "ATM", "CVS", "Barcode", "Credit", "ApplePay", "GooglePay"]
+    };
+  },
+  methods: {
+    submitForm() {
+      sessionStorage.setItem("form", JSON.stringify(this.form));
+      this.$router.push("/checkout");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.form-control {
+  background-color: transparent;
+  color: #fff;
+  &:focus {
+    background-color: transparent;
+  }
+}
+</style>
